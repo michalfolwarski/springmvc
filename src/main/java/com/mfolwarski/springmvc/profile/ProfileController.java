ca0280979dfa7d482fee85556b989114d1e11eee
Michał Folwarski <folwarski.michal@gmial.com>
2018-10-31 23:14:30 +0100

    date format placeholder

--- src/main/java/com/mfolwarski/springmvc/profile/ProfileController.java
+++ src/main/java/com/mfolwarski/springmvc/profile/ProfileController.java
@@ -1,13 +1,23 @@
 package com.mfolwarski.springmvc.profile;
 
+import com.mfolwarski.springmvc.date.LocalDateFormatter;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.util.Locale;
+
 @Controller
 public class ProfileController {
+
+    @ModelAttribute("dateFormat")
+    public String localeFormat(Locale locale){
+        return LocalDateFormatter.getPattern(locale).toUpperCase();
+    }
+
     @RequestMapping("/profile")
-    public String displayProfile(ProfileForm profileForm){
+    public String displayProfile(@ModelAttribute("profileForm") ProfileForm profileForm){
         return "profile/profilePage";
     }
 
