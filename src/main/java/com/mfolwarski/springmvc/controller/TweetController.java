418a387f4639d79168eed96b4d2831ca98a22c06
Michał Folwarski <folwarski.michal@gmial.com>
2018-10-31 19:34:22 +0100

    filter a search term, display an error message

--- src/main/java/com/mfolwarski/springmvc/controller/TweetController.java
+++ src/main/java/com/mfolwarski/springmvc/controller/TweetController.java
@@ -7,8 +7,11 @@ import org.springframework.social.twitter.api.Twitter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -25,7 +28,18 @@ public class TweetController {
         return "searchPage";
     }
 
-     @RequestMapping(value = "/result")
+    @RequestMapping(value = "/postSearch", method = RequestMethod.POST)
+    public String postSearch(HttpServletRequest request, RedirectAttributes redirectAttributes){
+        String search = request.getParameter("search");
+        if (search.toLowerCase().contains("struts")){
+            redirectAttributes.addFlashAttribute("error", "Try using something else instead!'");
+            return "redirect:/";
+        }
+        redirectAttributes.addAttribute("search", search);
+        return "redirect:result";
+    }
+
+    @RequestMapping(value = "/result")
     public String hello(@RequestParam(defaultValue = "SpringMVC") String search, Model model){
         SearchResults searchResults = twitter.searchOperations().search(search);
         List<Tweet> tweets = searchResults.getTweets();
