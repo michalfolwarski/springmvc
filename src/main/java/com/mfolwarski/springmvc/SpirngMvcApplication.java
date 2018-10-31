0a95a90986087ca72c8a6b28aaee376557bad94b
Michał Folwarski <folwarski.michal@gmial.com>
2018-10-31 13:19:38 +0100

    init

---
+++ src/main/java/com/mfolwarski/springmvc/SpirngMvcApplication.java
@@ -0,0 +1,12 @@
+package com.mfolwarski.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpirngMvcApplication.class, args);
+    }
+}
