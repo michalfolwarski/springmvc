a575ae0364202f27a720f1577bacec9ea66098b9
Michał Folwarski <folwarski.michal@gmial.com>
2018-11-02 16:18:37 +0100

    basic validation

--- src/main/java/com/mfolwarski/springmvc/profile/ProfileForm.java
+++ src/main/java/com/mfolwarski/springmvc/profile/ProfileForm.java
@@ -1,15 +1,23 @@
 package com.mfolwarski.springmvc.profile;
 
 import lombok.Data;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.NotEmpty;
 
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Data
 public class ProfileForm {
+    @Size(min = 3)
     private String twitterHandle;
+    @Email @NotEmpty
     private String email;
+    @NotNull
     private LocalDate birthDate;
+    @NotEmpty
     private List<String> tastes = new ArrayList<>();
 }
