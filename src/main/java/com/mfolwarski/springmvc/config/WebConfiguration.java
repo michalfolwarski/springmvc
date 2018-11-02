9b25b81a5f0a7b524ca5b095fe89113e54bb36f6
Michał Folwarski <folwarski.michal@gmial.com>
2018-11-02 19:25:40 +0100

    polish messages

--- src/main/java/com/mfolwarski/springmvc/config/WebConfiguration.java
+++ src/main/java/com/mfolwarski/springmvc/config/WebConfiguration.java
@@ -1,9 +1,14 @@
 package com.mfolwarski.springmvc.config;
 
 import com.mfolwarski.springmvc.date.LocalDateFormatter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.time.LocalDate;
 
@@ -12,5 +17,22 @@ public class WebConfiguration extends WebMvcConfigurerAdapter {
 
     @Override public void addFormatters(FormatterRegistry registry) {
         registry.addFormatterForFieldType(LocalDate.class, new LocalDateFormatter());
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new SessionLocaleResolver();
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName("lang");
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
